9cc58c219e4f3643a3f4d91e48c802bb74d33306
Aiden <fsd76005588@gmail.com>
2021-02-04 13:50:43 +0900

    [server] routefix

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const cookieParser = require("cookie-parser");
 const privateKey = fs.readFileSync(__dirname + "/key.pem", "utf8");
 const certificate = fs.readFileSync(__dirname + "/cert.pem", "utf8");
 const credentials = { key: privateKey, cert: certificate };
-// const usersRouter = require('./routes/user');
+const usersRouter = require('./routes/user');
 
 const express = require("express");
 const app = express();
-const controllers = require("./controller/users");
+// const controllers = require("./controller/users");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,10 +23,10 @@ app.use(cookieParser());
 // app.post("/login", controllers.login);
 // app.get("/accesstokenrequest", controllers.accessTokenRequest);
 // app.get("/refreshtokenrequest", controllers.refreshTokenRequest);
-// app.use('/users', usersRouter);
-app.post("/users/login", controllers.Login)
-app.post("/users/logout", controllers.Logout)
-app.post("/users/signup", controllers.Signup)
+app.use('/users', usersRouter);
+// app.post("/users/login", controllers.Login)
+// app.post("/users/logout", controllers.Logout)
+// app.post("/users/signup", controllers.Signup)
 app.get("/", (req,res) => {res.send('hello')})
 
 
