8ae34f107256bc4dcbd1c8f995ac95488ba96635
최재선 <js@choejaeseon-ui-MacBookPro.local>
2021-02-10 16:25:19 +0900

    a

--- server/app.js
+++ server/app.js
@@ -2,6 +2,9 @@ const fs = require("fs");
 const https = require("https");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const privateKey = fs.readFileSync(__dirname + "/../key.pem", "utf8");
+const certificate = fs.readFileSync(__dirname + "/../cert.pem", "utf8");
+const credentials = { key: privateKey, cert: certificate };
 const usersRouter = require('./routes/user');
 
 const express = require("express");
@@ -28,7 +31,7 @@ app.get("/", (req,res) => {res.send('hello')})
 
 
 const HTTPS_PORT = 4000;
-const httpsServer = https.createServer(app);
+const httpsServer = https.createServer(credentials, app);
 httpsServer.listen(HTTPS_PORT, () => console.log("server runnning", HTTPS_PORT));
 
 module.exports = httpsServer;
